257dc7e37f710bd042958699a45ae636c6ff0299
Denys Dziubii <ddziubii@sfu.ca>
2019-06-24 21:53:38 -0700

    In

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ express()
     let result = ''
     const times = process.env.TIMES || 5
     for (i = 0; i < times; i++) {
-      result += i + ' '
+      result += i + '    '
     }
     return result;
   }
