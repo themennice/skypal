ee100534d6bbd5a698b42682d031ef377e2c2ba4
aanvari <arash_anvari@sfu.ca>
2019-06-30 14:32:25 -0700

    Arash Anvari,Profilejs

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-  //ssl: true
-});
+//const { Pool } = require('pg');
+
+//   const pool = new Pool({
+//  connectionString: process.env.DATABASE_URL,
+//  ssl: true
+//});
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
+const app = express()
+const { Pool } = require('pg');
+var pool = new Pool({
+database: 'postgres'
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(express.json())
-  .use(express.urlencoded({extended:false}))
-  .post("/login", async (req, res) => {	
-	var uname = req.body.username;
-	var upass = req.body.password;
-	//console.log(uname);
-
-	try {
-        const client = await pool.connect()
-        const result = await client.query("SELECT * FROM users where username='" + uname + "'");
-		if (result.rows[0]) { 
-			if (result.rows[0].password == upass) {
-			
-				// ** Load main page here ** //
-
-				var user = {'user' : uname};
-				res.render("login", user);
-			
-			
-				// ** ******************* ** //
-				
-			} else {
-				res.send("Wrong password");
-			}
-		} else {
-			res.send("User not found");
-		}
-        
-		client.release();
-      } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-      }	
-   })
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/login', (req, res) => res.render('login'))
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/users', function (req, res) {
+    console.log('Hello');
+  	pool.query('select * from users', function(error, result){
+  		var results = { 'results': (result) ? result.rows : [] };
+  		res.render('profile',results);
+  	})
+  })
+  //.get('/', (req, res) => res.render('pages/index'))
   .get('/db', async (req, res) => {
       try {
         const client = await pool.connect()
