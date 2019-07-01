e47604d153a3e0af5a03f55cda644bbf5aefa875
jbiedka <jbiedka@sfu.ca>
2019-07-01 13:23:33 -0700

    google test

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.json())
   .use(express.urlencoded({extended:false}))
+  .post("/loginem", async (req, res) => {	
+	var email = req.body.key;
+	try {
+        const client = await pool.connect()
+        const result = await client.query("SELECT * FROM users where email='" + email + "'");
+		res.send(result.rows[0]);
+		client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }	
+   })
   .post("/login", async (req, res) => {	
 	var uname = req.body.username;
 	var upass = req.body.password;
@@ -21,20 +33,10 @@ express()
         const result = await client.query("SELECT * FROM users where username='" + uname + "'");
 		if (result.rows[0]) { 
 			if (result.rows[0].password == upass) {
-			
 				// ** Load main page here ** //
-
-				var user = {'user' : uname};
-				//res.render("login", user);
-				
-				
-				//res.send(result.rows)
-			
 				var results = { 'results': (result) ? result.rows : [] };
 				res.render('profile',results);
-			
 				// ** ******************* ** //
-				
 			} else {
 				res.send("Wrong password");
 			}
@@ -52,23 +54,4 @@ express()
   .set('view engine', 'ejs')
   .get('/login', (req, res) => res.render('login'))
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/users', function (req, res) {
-    console.log('Hello');
-  	pool.query('select * from users', function(error, result){
-  		var results = { 'results': (result) ? result.rows : [] };
-  		res.render('profile',results);
-  	})
-  })
-  .get('/db', async (req, res) => {
-      try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
-        client.release();
-      } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-      }
-    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
