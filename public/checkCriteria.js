35518480b228856b0edee55e337afd0e3863f113
Chloe Chan <chc70@sfu.ca>
2019-07-02 21:05:52 -0700

    check pw

--- public/checkCriteria.js
+++ public/checkCriteria.js
@@ -1,1 +1,6 @@
-function password check
+function passwordCheck(){
+	var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("confirmPassword");
+    if (password =! confirmPassword)
+        alert("password does not match!");
+}
