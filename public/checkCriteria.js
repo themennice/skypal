b0805c8f7585c86d0019bbf3cb667968ce94e2b9
Chloe Chan <chc70@sfu.ca>
2019-07-11 15:38:18 -0700

    finished checking email format, test on sending email to users code added

--- public/checkCriteria.js
+++ public/checkCriteria.js
@@ -1,13 +1,23 @@
 function validateForm(){
 	var password = document.forms["registerForm"]["password"].value;
     var confirmPassword = document.forms["registerForm"]["confirmPassword"].value;
+    var email = document.forms["registerForm"]["email"].value;
     //console.log(password);
     //console.log(confirmPassword);
     if (password == confirmPassword)
+    {
+        sendEmail(email);
         return true;
+    }
     else
     {
     	alert("password does not match!");
         return false;
     } 
+}
+
+function sendEmail(email)
+{
+    var link = "mailto:'"++"'"+ "&subject=" + escape("Test")+ "&body=" + escape("Testing");
+    window.location.href = link;
 }
