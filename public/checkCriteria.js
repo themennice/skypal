a189c9d9eebaa01938f15ed334f98e5fa26ac104
Chloe Chan <chc70@sfu.ca>
2019-07-11 16:13:35 -0700

    use api to send email to user

--- public/checkCriteria.js
+++ public/checkCriteria.js
@@ -2,11 +2,26 @@ function validateForm(){
 	var password = document.forms["registerForm"]["password"].value;
     var confirmPassword = document.forms["registerForm"]["confirmPassword"].value;
     var email = document.forms["registerForm"]["email"].value;
+    var mandrill = require('node-mandrill')('84ac4065a5019c7e1a8ab2a2096c3bf1-us3');
     //console.log(password);
     //console.log(confirmPassword);
     if (password == confirmPassword)
     {
-        sendEmail(email);
+        mandrill('/messages/send', {
+        message: {
+            to: [{email: email, name: 'Jim Rubenstein'}],
+            from_email: 'chc70@sfu.ca',
+            subject: "Test",
+            text: "Testing"
+            }
+        }, function(error, response)
+        {
+            //uh oh, there was an error
+            if (error) console.log( JSON.stringify(error) );
+
+            //everything's good, lets see what mandrill said
+            else console.log(response);
+        });
         return true;
     }
     else
@@ -18,6 +33,5 @@ function validateForm(){
 
 function sendEmail(email)
 {
-    var link = "mailto:'"+email+"'"+ "&subject=" + escape("Test")+ "&body=" + escape("Testing");
-    window.location.href = link;
+
 }
