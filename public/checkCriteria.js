4560766a9cbbcfbde4e9bc20a32fafc02c88281d
Chloe Chan <chc70@sfu.ca>
2019-07-11 19:08:20 -0700

    email api

--- public/checkCriteria.js
+++ public/checkCriteria.js
@@ -1,7 +1,7 @@
 function validateForm(){
 	var password = document.forms["registerForm"]["password"].value;
     var confirmPassword = document.forms["registerForm"]["confirmPassword"].value;
-    var email = document.forms["registerForm"]["email"].value;
+    var Uemail = document.forms["registerForm"]["email"].value;
     var mandrill = require('node-mandrill')('84ac4065a5019c7e1a8ab2a2096c3bf1-us3');
     //console.log(password);
     //console.log(confirmPassword);
@@ -9,7 +9,7 @@ function validateForm(){
     {
         mandrill('/messages/send', {
         message: {
-            to: [{email: email, name: 'Jim Rubenstein'}],
+            to: Uemail,
             from_email: 'chc70@sfu.ca',
             subject: "Test",
             text: "Testing"
