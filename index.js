cf30d5b3fa00623380626c1c3e6bba451fca3df7
Denys Dziubii <ddziubii@sfu.ca>
2019-07-15 22:04:02 -0700

    navigation bar reflects login via gloval variable + index.js is now partitioned into routes.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-  //ssl: true
-});
+//const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL
+// });
 const PORT = process.env.PORT || 5000
 const express = require('express')
-
-var flash = require('connect-flash');
-var passport = require('passport');
-var request = require('request');
-var session = require('express-session')
-var request = require('request');
-var bodyParser = require('body-parser');
+const request = require('request');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session')
+const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
-const path = require('path')
+const path = require('path');
 
 var app = express()
 app.use(passport.initialize())
@@ -30,52 +27,10 @@ app.use(session({
     resave: true,
     saveUninitialized: true
     }))
-app.set('view options', { layout: false }) // may be omitted
+app.set('view options', { layout: false })
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-app.post("/loginem", async (req, res) => { // what is loginem?
-  	var email = req.body.key;
-    console.log("login attempt 12");
-  	try {
-          const client = await pool.connect()
-          const result = await client.query("SELECT * FROM users where email='" + email + "'");
-
-  		console.assert(result.rows[0], { result : result.rows[0], error : "database error, user not found or is returning null" } );
-      console.log("login attempt 14");
-  		res.send(result.rows[0]);
-  		client.release();
-        } catch (err) {
-          console.error(err);
-          res.send("Error is " + err);
-        }
-     })
-app.post("/profile", async (req, res) => {
-  	  console.log(req.body);
-  	  console.log(req.body.Username);
-
-  	  try {
-          const client = await pool.connect()
-  		var test = "update users set name = '"+ req.body.Name + "', email = '"+ req.body.email + "', age = '"+ req.body.Age + "'where username = '" + req.body.Username + "'";
-          const update = await client.query(test);
-  		//{"Name":"Julian","Usename":"jbiedka","password":"123","email":"jbiedka@sfu.ca","Age":"19"}
-  		const result = await client.query("SELECT * FROM users where username='" + req.body.Username + "'");
-
-  		console.assert(result.rows[0], { result : result.rows[0], error : "database error, user not found or is returning null" } );
-
-  		res.render('profile', { 'r': result.rows[0] } );
-
-  		client.release();
-        } catch (err) {
-          console.error(err);
-          res.send("Error " + err);
-        }
-    })
-
-app.get('*', function (req, res, next) { // universal access variable, keep working
-  res.locals.user = req.user || null;
-  next();
-})
-
+global.user = false;
 require('./routes.js')(app);
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- routes.js
+++ routes.js
@@ -1,14 +1,14 @@
-var util = require('util');
-var express = require('express');
-var app = express();
-var passport = require('passport');
-
-var fs = require('fs');
-var request = require('request');
+// const util = require('util');
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const flash = require('connect-flash');
+const fs = require('fs');
+const request = require('request');
 const { Pool, Client } = require('pg')
-global.globalString = "This can be accessed anywhere!";
-const bcrypt = require('bcryptjs'); // Julian, do we need this? -> for storing passwords in a hashed format.
-const uuidv4 = require('uuid/v4');// uuid/v4? //used for generating universal unique IDs
+
+const bcrypt = require('bcryptjs');
+const uuidv4 = require('uuid/v4');
 
 const LocalStrategy = require('passport-local').Strategy; // strategy for authenticating with a username and password
 
@@ -19,11 +19,39 @@ const pool = new Pool({
 });
 
 module.exports = function (app) {
-
-  app.get('/', (req, res, next) => { res.render('pages/index', {title: "Homepage",
-        userData: req.user, messages: {danger: req.flash('danger'),
-        warning: req.flash('warning'), success: req.flash('success')}});
+  console.log("The status of user is " + user);
+  app.get('/', (req, res, next) => { res.render('pages/index');
         console.log("The user is "+ req.user); })
+
+  app.get('/register', (req, res) => res.render('register'))
+
+  app.get('/add-ticket', (req, res) => res.render('add-ticket'))
+
+  app.get('/login', (req, res) => {
+      console.log("Attempting to log in at /login");
+      //console.log(req.session.passport.user);
+      if (req.isAuthenticated()) {
+        user = true;
+        console.log("Get login user status is " + user);
+        console.log("login attempt 11");
+        res.redirect('/profile');}
+      else { res.render('login'); console.log("Not logged in, render the login page")}})
+
+  app.get('/logout', function(req, res){
+       user = false;
+       console.log("Upon logout user status is " + user);
+       console.log(req.isAuthenticated());
+       req.logout();
+       console.log(req.isAuthenticated());
+       req.flash('success', "Logged out. See you soon!");
+       res.redirect('/');
+       })
+
+   app.get('*', function (req, res, next) { // universal access variable, keep working
+     console.log(req.user);
+     res.locals.user = req.user || null;
+     console.log(res.locals.user);
+     next();})
 
   app.post('/register', async function(req, res)
     {
@@ -58,22 +86,42 @@ module.exports = function (app) {
           res.send("Error " + err);
         }
       })
-//
-    app.get('/register', (req, res) => res.render('register'))
-    app.get('/add-ticket', (req, res) => res.render('add-ticket'))
-    app.get('/login', (req, res) => {
-        console.log("Attempting to log in");
-        if (req.isAuthenticated()) {
-          console.log("login attempt 11");
-          res.redirect('/profile');}
-        else { res.render('login'); console.log("Not logged in, render the login page")}})
-    app.get('/logout', function(req, res){
-         console.log(req.isAuthenticated());
-         req.logout();
-         console.log(req.isAuthenticated());
-         req.flash('success', "Logged out. See you soon!");
-         res.redirect('/');
-         })
+    app.post("/profile", async (req, res) => {
+      	  console.log(req.body);
+      	  console.log(req.body.Username);
+
+      	  try {
+              const client = await pool.connect()
+      		var test = "update users set name = '"+ req.body.Name + "', email = '"+ req.body.email + "', age = '"+ req.body.Age + "'where username = '" + req.body.Username + "'";
+              const update = await client.query(test);
+      		//{"Name":"Julian","Usename":"jbiedka","password":"123","email":"jbiedka@sfu.ca","Age":"19"}
+      		const result = await client.query("SELECT * FROM users where username='" + req.body.Username + "'");
+
+      		console.assert(result.rows[0], { result : result.rows[0], error : "database error, user not found or is returning null" } );
+
+      		res.render('profile', { 'r': result.rows[0] } );
+
+      		client.release();
+            } catch (err) {
+              console.error(err);
+              res.send("Error " + err);
+            }
+        })
+
+    // app.get('/profile', async function (req, res) {
+    //     const client = await pool.connect()
+    //     const result = await client.query("SELECT * FROM users where username='" + "arash" + "'");
+    //     console.log(req.isAuthenticated());
+    //     console.log("Check here");
+    //     console.log(result.rows[0]);
+    //     res.render('profile', { 'r': result.rows[0] })})
+      // app.post('/login', (req, res) => {
+      //   passport.authenticate('local', {
+      //     successRedirect: '/profile',
+      //     failureRedirect: '/',
+      //     failureFlash: true
+      //   }) (req, res);
+      // })
      app.post('/login', passport.authenticate('local'),//, {
             // successRedirect: '/profile',
             // failureRedirect: '/',
@@ -96,6 +144,10 @@ module.exports = function (app) {
               		console.assert( uname != "" && upass != "", { username: uname, password: upass, error : "username and password can't be empty" } );
               		if ( (uname != "" && upass != "") && result.rows[0]) {
               			if (bcrypt.compare(upass, result.rows[0].password)) {
+                      console.log("why does not flash???");
+                      req.flash('danger', "Oops. Incorrect login details.");
+                      user = true;
+                      console.log("After login post user status is " + user);
               				res.render('profile', { 'r': result.rows[0] });
               			} else {
               				res.send("Wrong password");
@@ -108,6 +160,7 @@ module.exports = function (app) {
                       console.error(err);
                       res.send("Error " + err);
                     }
+
                  console.log("login attempt 10");
                });
 
@@ -118,37 +171,28 @@ module.exports = function (app) {
         console.log("login attempt");
       	async function loginAttempt() {
       		const client = await pool.connect()
-          console.log("login attempt 2");
       		try{
       			await client.query('BEGIN')
             console.log(username);
             // currentAccountsData array is empty, see in console.log. Why?
       			var currentAccountsData = await (client.query('SELECT username, name, email, password FROM users WHERE username=$1', [username], function(err, result) {
-              console.log("login attempt 3");
       				if(err) {
       					return done(err)}
-              console.log("login attempt 4");
       				if(result.rows[0] == null){
       					req.flash('danger', "Oops. Incorrect login details.");
       					return done(null, false, {message: 'No user found'});}
       				else{
                 console.log(result.rows[0]);
-                if(password == result.rows[0].password){
-                  console.log("TRUE");}
+                req.flash('danger', "Oops. Incorrect login details.");
       					bcrypt.compare(password, result.rows[0].password, function(err, isMatch) {
-                //if(password == result.rows[0].password) {
-                  console.log("Bcrypt compare login attempt 5");
-      						if (err){
-      							console.log('Error while checking password');
-      							return done();}
+                  console.log("Bcrypt compare login attempt");
+      						if (err) throw err;
       						else if (isMatch){
                     console.log("Passwords matched!");
-                    console.log("login attempt 6");
                     return done(null, [{email: result.rows[0].email, firstName: result.rows[0].firstName}]);}
       						else{
                     console.log("login attempt 7");
       							req.flash('danger', "Oops. Incorrect login details.");
-                    console.log("login attempt 15");
                     return done(null, false);}
       					});
       				}
