a7103239faf73cbefebd987e3f90ff694db57d3b
jbiedka <jbiedka@sfu.ca>
2019-07-16 21:41:11 -0700

    a

--- routes.js
+++ routes.js
@@ -90,6 +90,7 @@ module.exports = function (app) {
            })
        })
   app.post('/googlelogin', async function(req, res) {
+	  console.log(req.body)
 		var token = req.body.idtoken //this is probably right
 		const client = await pool.connect();
 		const result = await client.query("SELECT * from users where username='" + idtoken + "'");
