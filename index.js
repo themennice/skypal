b2d57d3ad3fccbf6fde2ceadd24dab95effe9798
Chloe Chan <chc70@sfu.ca>
2019-07-16 19:44:08 -0700

    solve merge conflict

--- index.js
+++ index.js
@@ -1,52 +1,11 @@
-//const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
-const PORT = process.env.PORT || 5000
-<<<<<<< HEAD
-const express = require('express')
-const request = require('request');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session')
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs');
-const path = require('path');
-
-var app = express()
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(bodyParser.json());
-app.use(require('body-parser').urlencoded({ extended: true }))
-app.use(require('cookie-parser')())
-app.use(cookieParser('secretString'));
-app.use(session({
-    secret: 'bulky keyboard',
-    resave: true,
-    cookie: { maxAge: 120000 },
-    saveUninitialized: true
-    }))
-app.use(flash())
-
-app.use(function(req, res, next){
-    res.locals.success_messages = req.flash('success_messages');
-    res.locals.error_messages = req.flash('error_messages');
-    res.locals.warning = req.flash('warning'); // how to make it work?
-    next();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+  //ssl: true
 });
-
-app.set('view options', { layout: false })
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-global.users = false;
-require('./routes.js')(app);
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-=======
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 var rand,host,link;
 
 express()
@@ -230,4 +189,3 @@ express()
   .get('/login', (req, res) => res.render('login'))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
->>>>>>> email-validate
