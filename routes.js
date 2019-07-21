9ed92cd51e9f0fd6c391c0b64c4abce1104e8cad
jbiedka <jbiedka@sfu.ca>
2019-07-21 16:28:29 -0700

    login test

--- routes.js
+++ routes.js
@@ -21,15 +21,15 @@ const pool = new Pool({
 });
 
 module.exports = function (app) {
-  //console.log("The status of users is " + users);
+  console.log("The status of users is " + users);
   app.get('*', function (req, res, next) { // universal access variable, keep working
-     ////console.log("The user is currently " + req.isAuthenticated());
+     //console.log("The user is currently " + req.isAuthenticated());
      res.locals.user = req.user || null;
-     ////console.log("The locals.user is " + res.locals.user);
+     //console.log("The locals.user is " + res.locals.user);
      next();})
 
   app.get('/', (req, res, next) => { res.render('pages/index', {title: "Home", userData: req.user, message: 'Success'});
-        ////console.log("The user  in '/' is "+ req.user); })
+        //console.log("The user  in '/' is "+ req.user); })
 
   app.get('/chat', function(req, res) {
       res.render('chat');
@@ -40,34 +40,34 @@ module.exports = function (app) {
   app.get('/add-ticket', (req, res) => res.render('add-ticket'))
 
   app.get('/profile', function (req, res, next) {
-        //console.log(req.isAuthenticated())
+        console.log(req.isAuthenticated())
         if(req.isAuthenticated()){
-          ////console.log("I am here");
+          //console.log("I am here");
           res.render('profile', {title: "Profile", userData: req.user, userData: req.user, messages: {danger: req.flash('danger'), warning: req.flash('warning'), success: req.flash('success')}});
         }
         else{
-		  ////console.log("login failed")
+		  //console.log("login failed")
           res.redirect('/login');
         }
         });
 
   app.get('/login', (req, res, next) => {
-      ////console.log(res)
-	  ////console.log(typeof res)
+      //console.log(res)
+	  //console.log(typeof res)
       if (req.isAuthenticated()) {
         users = true;
-        ////console.log("Get login user status is " + users);
-        ////console.log("login attempt 11");
+        //console.log("Get login user status is " + users);
+        //console.log("login attempt 11");
         res.redirect('/profile');}
       else { res.render('login', {title: "Log in", userData: req.user, messages: {danger: req.flash('danger'), warning: req.flash('warning'), success: req.flash('success')}});
-        ////console.log("Not logged in, render the login page")}})
+        //console.log("Not logged in, render the login page")}})
 
   app.get('/logout', function(req, res){
        users = false;
-       ////console.log("Upon logout user status is " + users);
-       ////console.log(req.isAuthenticated());
+       //console.log("Upon logout user status is " + users);
+       //console.log(req.isAuthenticated());
        req.logout();
-       ////console.log(req.isAuthenticated());
+       //console.log(req.isAuthenticated());
        req.flash('success', "Logged out. See you soon!");
        res.redirect('/'); })
 
@@ -86,28 +86,26 @@ module.exports = function (app) {
            var sql = "INSERT INTO tickets (fname,lname,flightno,countryfrom,countryto,airline,date,time,username) VALUES ('"+fname+"','"+lname+"','"+flightno+"','"+countryfrom+"','"+countryto+"','"+airline+"','"+date+"','"+time+"','"+uname+"')";
            pool.query(sql, function(error,result){
              if (error){
-               //console.log(error);
+               console.log(error);
              }
              else {
-               //console.log("ticket added");
+               console.log("ticket added");
                res.redirect('/');
              }
            })
        })
   app.post('/googlelogin', async function(req, res) {
 	var token = req.body.idtoken //this is probably right
-	//console.log(token)
-	////console.log(res)
-	//console.log(typeof res)
+	console.log(token)
+	//console.log(res)
+	console.log(typeof res)
 	try {
 		const client = await pool.connect();
 		const result = await client.query("SELECT * from users where username='" + token + "'");
 		
 		if (result.rows[0]) {
-			console.log("initiate login")
 			res.render('profile', { 'r': result.rows[0] });
 		} else {
-			console.log("initiate add and login")
 			client.query("INSERT INTO users (username, password, email) VALUES ('" + token + "', '', '')");
 			const update = await client.query("SELECT * from users where username='" + token + "'");
 			res.render('profile', { 'r': update.rows[0] });
@@ -119,17 +117,17 @@ module.exports = function (app) {
   app.post('/register', async function(req, res)
     {
       var emailAddr = req.body.email;
-      //console.log("The email address is " + emailAddr);
+      console.log("The email address is " + emailAddr);
       var uName = req.body.username;
 
       try {
           const client = await pool.connect();
           var pwd = await bcrypt.hash(req.body.password, 5);
-          // //console.log("The password is " + req.body.password);
-          // //console.log("The hashed password is " + pwd);
-          // //console.log(typeof(pwd));
+          // console.log("The password is " + req.body.password);
+          // console.log("The hashed password is " + pwd);
+          // console.log(typeof(pwd));
   		// VALIDATE AND REDIRECT
-          // //console.log("message here");
+          // console.log("message here");
           const result = await client.query("SELECT * FROM users where username='" + uName + "'");
 
   		console.assert(!result.rows[0], { result : result.rows[0], error : "User already exists" } );
@@ -177,8 +175,8 @@ module.exports = function (app) {
       })
 
     app.post("/profile", async (req, res) => {
-      	  //console.log(req.body);
-      	  //console.log(req.body.Username);
+      	  console.log(req.body);
+      	  console.log(req.body.Username);
 
       	  try {
               const client = await pool.connect()
@@ -201,8 +199,8 @@ module.exports = function (app) {
 
     app.post('/googlelogin', async function(req, res) {
 	var token = req.body.idtoken //this is probably right
-	//console.log(token)
-	////console.log(res)
+	console.log(token)
+	//console.log(res)
 	try {
 		const client = await pool.connect();
 		const result = await client.query("SELECT * from users where username='" + token + "'");
@@ -224,20 +222,20 @@ module.exports = function (app) {
             // failureRedirect: '/',
             // failureFlash: true}),
             async function(req, res) {
-              //console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-              //console.log(req.isAuthenticated())
-              //console.log("login attempt 8");
+              console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+              console.log(req.isAuthenticated())
+              console.log("login attempt 8");
               if (req.body.remember) {
-                //console.log("Checking remember is " + req.body.remember);
+                console.log("Checking remember is " + req.body.remember);
                 req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
               }
               else {
-                //console.log("login attempt 9");
+                console.log("login attempt 9");
                 req.session.cookie.expires = false; // Cookie expires at end of session
                 }
               var uname = req.body.username;
               var upass = req.body.password;
-              	//console.log(uname);
+              	console.log(uname);
               	try {
                       const client = await pool.connect()
                       const result = await client.query("SELECT * FROM users where username='" + uname + "'");
@@ -245,11 +243,11 @@ module.exports = function (app) {
               		console.assert( uname != "" && upass != "", { username: uname, password: upass, error : "username and password can't be empty" } );
               		if ( (uname != "" && upass != "") && result.rows[0]) {
               			if (bcrypt.compare(upass, result.rows[0].password)) {
-                      //console.log("why does not flash???");
+                      console.log("why does not flash???");
                       req.flash('danger', "Oops. Incorrect login details.");
                       users = true;
-                      ////console.log("After login post user status is " + users);
-                      //console.log(req.isAuthenticated())
+                      //console.log("After login post user status is " + users);
+                      console.log(req.isAuthenticated())
                       res.render('profile', { 'c': result_ticket.rows,'r': result.rows[0] });
               				//res.render('profile', { 'r': result.rows[0] });
               			} else {
@@ -264,7 +262,7 @@ module.exports = function (app) {
                       res.send("Error " + err);
                     }
 
-                 //console.log("login attempt 10");
+                 console.log("login attempt 10");
                });
 }
 
@@ -272,13 +270,13 @@ module.exports = function (app) {
       passport.use('local', new  LocalStrategy({passReqToCallback : true}, (req, username, password, done) => {
 
       	loginAttempt();
-        //console.log("login attempt");
+        console.log("login attempt");
       	async function loginAttempt() {
       		const client = await pool.connect()
       		try{
       			await client.query('BEGIN')
-            //console.log("CURRENT USERNAME IS " + username);
-            // currentAccountsData array is empty, see in //console.log. Why?
+            console.log("CURRENT USERNAME IS " + username);
+            // currentAccountsData array is empty, see in console.log. Why?
       			var currentAccountsData = await JSON.stringify(client.query('SELECT username, name, email, password FROM users WHERE username=$1', [username], function(err, result) {
       				if(err) {
       					return done(err)}
@@ -286,16 +284,16 @@ module.exports = function (app) {
       					req.flash('danger', "Oops. Incorrect login details.");
       					return done(null, false, {message: 'No user found'});}
       				else{
-                //console.log("INSIDE BCRYPT");
-                //console.log(result.rows[0]);
+                console.log("INSIDE BCRYPT");
+                console.log(result.rows[0]);
       					bcrypt.compare(password, result.rows[0].password, function(err, isMatch) {
-                  //console.log("Bcrypt compare login attempt");
+                  console.log("Bcrypt compare login attempt");
       						if (err) throw err;
       						else if (isMatch){
-                    //console.log("Passwords matched!");
+                    console.log("Passwords matched!");
                     return done(null, [{email: result.rows[0].email, name: result.rows[0].name, username: result.rows[0].username}]);}
       						else{
-                    //console.log("login attempt 7");
+                    console.log("login attempt 7");
       							req.flash('danger', "Oops. Incorrect login details.");
                     return done(null, false);}
       					});
