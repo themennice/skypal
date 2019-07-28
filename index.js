a60651b25fd638b2157cdcf25191af3ae67fcb35
jbiedka <jbiedka@sfu.ca>
2019-07-28 16:34:31 -0700

    npm update

--- index.js
+++ index.js
@@ -237,15 +237,15 @@ const pool = new Pool({
 		// 4. This will be called after the response is received
 		xhr.onload = function() {
 		  if (xhr.status != 200) { // analyze HTTP status of the response
-			alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+			//alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
 		  } else { // show the result
-			alert(`Done, got ${xhr.response.length} bytes`); // responseText is the server
+			//alert(`Done, got ${xhr.response.length} bytes`); // responseText is the server
 			res.send(xhr.responseText);
 		  }
 		};
 
 		xhr.onerror = function() {
-		  alert("Request failed");
+		  //alert("Request failed");
 		};
 		
 		
